d0236fce7a447427ad919f2ab9a5f3b491789b52
zackckpetersen <zackcpetersen@gmail.com>
2020-11-24 16:13:51 -0700

    updating media root

--- images/models.py
+++ images/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Image(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
