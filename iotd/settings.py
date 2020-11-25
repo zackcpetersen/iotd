46a8f3064513bb2d8a2fa3ac3364a6de4d7fe8c5
zackckpetersen <zackcpetersen@gmail.com>
2020-11-25 12:39:48 -0700

    final updates

--- iotd/settings.py
+++ iotd/settings.py
@@ -157,3 +157,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+if 'AWS_ACCESS_KEY_ID' in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+
+
+"""
+Use this as a template to add environment variables with the CLI
+--------------------------------------------------------------
+eb setenv \
+AWS_ACCESS_KEY_ID='' \
+AWS_SECRET_ACCESS_KEY='' \
+AWS_STORAGE_BUCKET_NAME='' \
+AWS_S3_REGION_NAME=''
+"""
